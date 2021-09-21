38bc6d753d1421d0fc75fa086bb5ac2b732a7df7
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-21 15:24:34 +0530

    Added a single element outline of the page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
 function App() {
-  const [loading,setLoading] = useState(true);
-  const [jobs,setJobs] = useState([]);
-  const [value,setValue] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
   const fetchJobs = async () => {
     const response = await fetch(url);
     const jobsData = await response.json();
     setJobs(jobsData);
     setLoading(false);
-  }
+  };
 
   useEffect(() => {
     fetchJobs();
-  } , [])
-  if(loading){
-    return <section className="section loading">
-      <h1>Loading....</h1>
-      </section>;
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading....</h1>
+      </section>
+    );
   }
-  return <h2>Jobs</h2>
+  //must be written here as this must be done after loading cuz before loading the array is empty.
+  const { id, title, dates, duties, company } = jobs[value];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/*btn Container */}
+        {/* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty,index) => {
+            return(
+              <div className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  )
 }
 
-export default App
+export default App;
